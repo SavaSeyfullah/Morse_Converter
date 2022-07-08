05aa1cabb869a2be6c2758cf9cb15d56d45a0cb0
SavaSeyfullah <88098293+SavaSeyfullah@users.noreply.github.com>
2022-07-08 11:22:04 +0300

    Add files via upload

---
+++ Proje1.py
@@ -0,0 +1,33 @@
+MORSE__DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'} 
+
+              
+def to_morse(word):
+  text_morse = []
+  word_split = (list(word.upper()))
+  for letter in word_split:
+    for code in MORSE_DICT:
+      if code == letter:
+        morse_code = MORSE_DICT[code]
+    text_morse.append(letter.replace(letter,morse_code))
+    print(text_morse)
+
+
+
+text = input("Kelime gir: ")
+to_morse(text)
+      
+
